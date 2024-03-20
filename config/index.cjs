cad8f69c8d11981993809612a9c270a4422021b6
wl <xxzhwl@163.com>
2024-03-20 11:15:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,67 +1,46 @@
-/* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    "APP_ID": "wx100d315e54a6faf7",
+    "APP_SECRET": "62133a6462dd4d0b53c864ee1bc557a6",
+    "IS_SHOW_COLOR": false,
+    "CALLBACK_TEMPLATE_ID": "",
+    "CALLBACK_USERS": [
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+            "name": "自己",
+            "id": ""
+        }
+    ],
+    "USERS": [
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+            "name": "wl",
+            "id": "ogdOk6kcl_flF1xMaZsN6BvCcTXw",
+            "useTemplateId": "8Htq2iLpYTFHZQBcuerNsFjHU-PZwEq4qvf_vK_AW_g",
+            "province": "广东",
+            "city": "深圳",
+            "horoscopeDate": "03-01",
+            "horoscopeDateType": "今日",
+            "openUrl": "www.bilibili.com",
+            "festivals": [
+                {
+                    "type": "生日",
+                    "name": "www",
+                    "date": "07-02",
+                    "year": "1997"
+                }
+            ],
+            "customizedDateList": []
+        }
+    ],
+    "SWITCH": {
+        "weather": true,
+        "holidaytts": true,
+        "CIBA": true,
+        "oneTalk": true,
+        "earthyLoveWords": true,
+        "momentCopyrighting": true,
+        "poisonChickenSoup": false,
+        "poetry": false,
+        "horoscope": false,
+        "birthdayMessage": true
     }
-  ],
-
-}
-
-module.exports = USER_CONFIG
+}module.exports = USER_CONFIG
 
